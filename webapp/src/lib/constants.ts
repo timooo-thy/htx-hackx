cc435ee600083c310dca04dc954369d16336a6be
Timothy Lee <timothylhy@hotmail.com>
2024-10-24 16:01:06 +0800

    Added test metrics

--- webapp/src/lib/constants.ts
+++ webapp/src/lib/constants.ts
@@ -1,1 +1,7 @@
-const SEGMENT_API_URL = "https://api-qforensic.ngrok.app";
+export const SEGMENT_API_URL = "https://api-qforensic.ngrok.app";
+export const TEST_IMAGES = [
+  "/test_result/metrics_1.jpg",
+  "/test_result/metrics_2.jpg",
+  "/test_result/test_before.jpg",
+  "/test_result/test_after.jpg",
+];
